000b719c05177067720e14e2a08dcd5f271d0d51
ayoubalhakim72-ux <ayoubalhakim72@gmail.com>
2026-07-31 15:07:56 +0300

    Configure ticket assignment history model

--- smartdesk-laravel/app/Models/TicketHistory.php
+++ smartdesk-laravel/app/Models/TicketHistory.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketHistory extends Model
 {
-    //
+    protected $table = 'tickets_history';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ticketid',
+        'assignedby',
+        'assignedto',
+        'assigneddate',
+        'reason'
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class, 'ticketid');
+    }
+
+    public function assignedBy()
+    {
+        return $this->belongsTo(User::class, 'assignedby');
+    }
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assignedto');
+    }
 }
