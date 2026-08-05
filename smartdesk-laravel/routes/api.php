e17226688d1bbdfc178c52f381fb66b6507e65d6
ayoubalhakim72-ux <ayoubalhakim72@gmail.com>
2026-08-05 16:34:51 +0300

    Register user management and profile APIs

--- smartdesk-laravel/routes/api.php
+++ smartdesk-laravel/routes/api.php
@@ -6,19 +6,34 @@ use App\Http\Controllers\Api\TicketController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PriorityController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\StatusController;
 use App\Http\Controllers\Api\TicketCommentController;
+use App\Http\Controllers\Api\UserController;
 
 // Public route
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
-    // Authentication
+    // Authentication and profile
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+    Route::get('/profile', [ProfileController::class, 'show']);
+    Route::put('/profile', [ProfileController::class, 'update']);
 
     Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    // Admin user management
+    Route::middleware('role:Admin')->group(function () {
+        Route::get('/roles', [UserController::class, 'roles']);
+        Route::get('/users', [UserController::class, 'index']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::get('/users/{id}', [UserController::class, 'show']);
+        Route::put('/users/{id}', [UserController::class, 'update']);
+        Route::put('/users/{id}/ban', [UserController::class, 'setBan']);
+        Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    });
 
     // Tickets
     Route::get('/agents', [TicketController::class, 'agents']);
@@ -38,11 +53,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/priorities', [PriorityController::class, 'index']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/statuses', [StatusController::class, 'index']);
-});
-
-// Admin test
-Route::middleware(['auth:sanctum', 'role:Admin'])->get('/admin-test', function () {
-    return response()->json([
-        'message' => 'Welcome Admin!'
-    ]);
 });
