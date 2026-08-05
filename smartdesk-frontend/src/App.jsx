cbb4e2fe2cdfd3e86028d7f153bcdbbb0e3fc653
ayoubalhakim72-ux <ayoubalhakim72@gmail.com>
2026-08-05 16:34:35 +0300

    Register users and profile pages

--- smartdesk-frontend/src/App.jsx
+++ smartdesk-frontend/src/App.jsx
@@ -9,36 +9,59 @@ import Assignmen from "./pages/Assignmen";
 import TicketDetails from "./pages/TicketDetails";
 import TicketComments from "./pages/TicketComments";
 import TicketActivity from "./pages/TicketActivity";
+import Users from "./pages/Users";
+import CreateUser from "./pages/CreateUser";
+import EditUser from "./pages/EditUser";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/tickets" element={<Tickets />} />
+      <Route path="/create-ticket" element={<CreateTicket />} />
+      <Route path="/tickets/edit/:id" element={<EditTicket />} />
+      <Route path="/tickets/assign/:id" element={<Assignmen />} />
+      <Route path="/tickets/:id" element={<TicketDetails />} />
+      <Route path="/tickets/:id/comments" element={<TicketComments />} />
+      <Route path="/tickets/:id/activity" element={<TicketActivity />} />
 
-    return (
-
-        <Routes>
-
-            <Route path="/" element={<Login />} />
-
-            <Route path="/dashboard" element={<Dashboard />} />
-
-            <Route path="/tickets" element={<Tickets />} />
-
-            <Route path="/create-ticket" element={<CreateTicket />} />
-
-            <Route path="/tickets/edit/:id" element={<EditTicket />} />
-
-            <Route path="/tickets/assign/:id" element={<Assignmen />} />
-
-            <Route path="/tickets/:id" element={<TicketDetails />} />
-
-            <Route path="/tickets/:id/comments" element={<TicketComments />} />
-
-            <Route path="/tickets/:id/activity" element={<TicketActivity />} />
-            
-
-        </Routes>
-
-    );
-
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute allowedRoles={["Admin"]}>
+            <Users />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/create"
+        element={
+          <ProtectedRoute allowedRoles={["Admin"]}>
+            <CreateUser />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/:id/edit"
+        element={
+          <ProtectedRoute allowedRoles={["Admin"]}>
+            <EditUser />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
