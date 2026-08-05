2417a2277840c2ad8b25c80eb93d332515ac874f
ayoubalhakim72-ux <ayoubalhakim72@gmail.com>
2026-08-05 15:23:05 +0300

    Configure ticket activity log model

--- smartdesk-laravel/app/Models/ActivityLog.php
+++ smartdesk-laravel/app/Models/ActivityLog.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
-    //
+    protected $table = 'activity_logs';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ticketid',
+        'user_id',
+        'action',
+        'date'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime'
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class, 'ticketid');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
